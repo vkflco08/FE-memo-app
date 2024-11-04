05c12960bb1cd32713423a58048d4240c1ccc421
vkflco08 <vkflco08@naver.com>
2024-11-04 20:06:38 +0900

    style font size

--- src/components/my-info/MyInfo.jsx
+++ src/components/my-info/MyInfo.jsx
@@ -4,9 +4,9 @@ import './MyInfo.css'; // 스타일 파일을 추가할 수 있습니다.
 const ComingSoon = () => {
   return (
     <div className="coming-soon-container">
-      <h1>내정보 페이지를 준비중입니다!</h1>
-      <p>이 페이지는 곧 사용할 수 있게 됩니다. 조금만 기다려 주세요.</p>
-      <p>감사합니다!</p>
+      <h1 className='comming-soon-title'>내정보 페이지를 준비중입니다!</h1>
+      <p className='comming-soon-content'>이 페이지는 곧 사용할 수 있게 됩니다. 조금만 기다려 주세요.</p>
+      <p className='comming-soon-content'>감사합니다!</p>
     </div>
   );
 };
